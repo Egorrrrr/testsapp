f895e75556104530f6c3df2e8b7ce1472d5f35b4
egor.khlynin <egor.khlynin@exactprosystems.com>
2022-12-21 13:26:28 +0300

    init

---
+++ src/main/java/com/tests/testsapp/LoginController.java
@@ -0,0 +1,13 @@
+package com.tests.testsapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
---
+++ src/test/java/com/tests/testsapp/TestsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.testsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
