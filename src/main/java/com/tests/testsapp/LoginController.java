cfcebcb99f00701c2137c6647088c51ca391d273
Кирилл Малышев <kirillae21@gmail.com>
2022-12-22 13:31:40 +0300

    Login page

--- src/main/java/com/tests/testsapp/LoginController.java
+++ src/main/java/com/tests/testsapp/LoginController.java
@@ -1,13 +1,15 @@
 package com.tests.testsapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping(value = {"/", "/login"})
+    public String index(Model model) {
+        return "login";
     }
+
 }
