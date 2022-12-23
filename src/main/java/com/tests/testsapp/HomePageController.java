f636a1967c3859e9b3e4251e7b2059d5bf687141
Кирилл Малышев <kirillae21@gmail.com>
2022-12-23 19:31:13 +0300

    Main page

--- src/main/java/com/tests/testsapp/HomePageController.java
+++ src/main/java/com/tests/testsapp/HomePageController.java
@@ -1,19 +1,20 @@
 package com.tests.testsapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomePageController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
         return "home";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public String admin(Model model){
         return "home";
     }
 }
