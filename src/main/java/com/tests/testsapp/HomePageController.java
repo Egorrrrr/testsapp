f84cb3c38a94aec7d8f275feda429150b5046796
Кирилл Малышев <kirillae21@gmail.com>
2022-12-25 20:39:13 +0300

    Add question types handling in frontend

--- src/main/java/com/tests/testsapp/HomePageController.java
+++ src/main/java/com/tests/testsapp/HomePageController.java
@@ -8,8 +8,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomePageController {
@@ -37,5 +40,32 @@ public class HomePageController {
             return "admin";
         }
         return "403";
+    }
+
+    @GetMapping("/create-test")
+    public String createTest(Model model){
+
+
+        List<LabelValue> questionTypes = new ArrayList<>();
+        questionTypes.add(new LabelValue("q1", "RAX"));
+        questionTypes.add(new LabelValue("q2", "DA"));
+        questionTypes.add(new LabelValue("q3", "NEW"));
+
+        model.addAttribute("questionTypes", questionTypes);
+        return "create-test";
+    }
+
+    @RequestMapping(value = "/getType", method = RequestMethod.POST)
+    public String getType(){
+        return "fragments/q1";
+    }
+
+    private class LabelValue {
+        public LabelValue(String value, String label) {
+            this.value = value;
+            this.label = label;
+        }
+        public String value;
+        public String label;
     }
 }
